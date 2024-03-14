e88df9c699cf1c63b733cc8bdc669a3ec6e76040
Jackson Barreto <jackson.barreto@gmail.com>
2024-03-14 11:22:35 +0000

    fix: add more logs

--- cmd/dnssecanalyser/main.go
+++ cmd/dnssecanalyser/main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/jacksonbarreto/WebGateScanner-DNSSECAnalyzer/config"
 	"github.com/jacksonbarreto/WebGateScanner-DNSSECAnalyzer/internal/groupHandler"
 	"github.com/jacksonbarreto/WebGateScanner-DNSSECAnalyzer/internal/scanner"
+	"github.com/jacksonbarreto/WebGateScanner-DNSSECAnalyzer/pkg/logservice"
 	"github.com/jacksonbarreto/WebGateScanner-kafka/consumer"
 	"github.com/jacksonbarreto/WebGateScanner-kafka/producer"
 )
@@ -13,6 +14,8 @@ const configFilePath = ""
 
 func main() {
 	config.InitConfig(configFilePath)
+	logger := logservice.NewLogServiceDefault()
+	logger.Info("Starting DNSSEC Analyzer")
 	dnsScanner := scanner.NewScannerDefault()
 
 	kafkaProducer, producerErr := producer.NewProducer(config.Kafka().TopicProducer, config.Kafka().Brokers,
@@ -25,6 +28,7 @@ func main() {
 	handler := groupHandler.NewAnalysisConsumerGroupHandlerDefault(dnsScanner, kafkaProducer)
 
 	kafkaConfig := config.Kafka()
+	logger.Info("Starting consumer for topics: %v", kafkaConfig.TopicsConsumer)
 	kafkaConsumer, consumerErr := consumer.NewConsumer(kafkaConfig.Brokers, kafkaConfig.GroupID,
 		kafkaConfig.TopicsConsumer, handler, context.Background())
 	if consumerErr != nil {
